e10f50fbf6c82411ac5280798f0bf2da60982cf8
The Ace <udonsichisom02@gmail.com>
2023-05-20 00:34:29 +0100

    update: added production files

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "./rest-countries-api-with-color-theme-switcher/dist/",
-  },
   plugins: [react()],
 });
